2bb34417f8553ef95f636b575d9c80ad5eed5b43
Andres0x90 <74370272+Andres0x90@users.noreply.github.com>
2022-03-05 11:53:47 -0500

    Updated for letters puzzle

--- puzzle-bni.py
+++ puzzle-bni.py
@@ -1,12 +1,17 @@
 import random
 
-initial_state = [7,2,4,5,0,6,8,3,1]
-target = [0,1,2,3,4,5,6,7,8]
+initial_state = ['C', 'A', ' ', 'B']
+target = ['B', 'C', ' ', 'A']
+initial_state_matriz = [[initial_state[0], initial_state[1]], [initial_state[2], initial_state[3]]]
+target_state_matriz = [[target[0], target[1]], [target[2], target[3]]]
 
-print(f"Initial State: {initial_state}")
-print(f"Target: {target}")
+print(f"Initial State: {initial_state_matriz}")
+print(f"Target: {target_state_matriz}")
+random.shuffle(initial_state)
 
 while(initial_state != target):
     random.shuffle(initial_state)
+    initial_state_matriz = [[initial_state[0], initial_state[1]], [initial_state[2], initial_state[3]]]
+    
 
-print(f"Target found: {initial_state}")
+print(f"Target found: {initial_state_matriz}")
